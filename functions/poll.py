e5c1c2339a310e566a01016f419f6f29ed23b10b
Nika Kutsniashvili <nickberckley@gmail.com>
2024-07-24 13:44:31 +0400

    Fix: Errors for poll when there is no active object

--- functions/poll.py
+++ functions/poll.py
@@ -6,9 +6,8 @@ import bpy
 def shape_key_poll(context):
     """Check that context object has at least one more shape key besides basis"""
     obj = context.object
-    shape_keys = obj.data.shape_keys
 
-    if (obj and obj.type == 'MESH') and (shape_keys and len(shape_keys.key_blocks) >= 2):
+    if (obj and obj.type == 'MESH') and (obj.data.shape_keys and len(obj.data.shape_keys.key_blocks) >= 2):
         return True
     else:
         return False
