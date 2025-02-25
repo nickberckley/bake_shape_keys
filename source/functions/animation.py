1e503a01270a06880f343ad3d0d0c838deec9bf5
Nika Kutsniashvili <nickberckley@gmail.com>
2025-02-26 03:29:18 +0400

    Transfer f-curve properties, modifiers, and others when transferring animation
    Previously code was hard-coded (and very inefficient) to copy only selected keyframe properties, and everything else was ignored. It now copies everything: f-curve properties (such as extrapolation, color mode, lock, mute), it's action group, and it's modifiers as well as more efficiently transferring keyframes and their properties.

--- source/functions/animation.py
+++ source/functions/animation.py
@@ -4,7 +4,7 @@ import bpy
 #### ------------------------------ FUNCTIONS ------------------------------ ####
 
 def transfer_animation(shape_keys, source, *targets):
-    """Transfers animation from one shape key to another"""
+    """Transfers animation (f-curve properties, keyframes, f-curve modifiers, and drivers) from one shape key to another"""
 
     anim_data = shape_keys.animation_data
     if anim_data is None:
@@ -12,35 +12,49 @@ def transfer_animation(shape_keys, source, *targets):
 
     # Transfer F-Curves
     if anim_data.action is not None:
-        original_fcurve = None
         for fcurve in anim_data.action.fcurves:
             if fcurve.data_path == f'key_blocks["{source.name}"].value':
-                original_fcurve = fcurve
-                for keyframe in fcurve.keyframe_points:
-                    for target in targets:
-                        target.value = keyframe.co[1]
-                        target.keyframe_insert(data_path="value", frame=int(keyframe.co[0]))
+                # get_original_fcurve_properties
+                original_fcurve_group = fcurve.group.name if fcurve.group else ""
+                original_fcurve_properties = {prop.identifier: getattr(fcurve, prop.identifier)
+                                              for prop in fcurve.bl_rna.properties if not prop.is_readonly}
 
-            # transfer_properties_of_original_keyframes
-            target_fcurves = {f'key_blocks["{target.name}"].value' for target in targets}
-            if fcurve.data_path in target_fcurves:
+                # get_original_keyframes
+                original_keyframes = {}
                 for keyframe in fcurve.keyframe_points:
-                    frame = int(keyframe.co[0])
-                    if original_fcurve:
-                        for original_keyframe in original_fcurve.keyframe_points:
-                            if int(original_keyframe.co[0]) == frame:
-                                keyframe.interpolation = original_keyframe.interpolation
-                                keyframe.easing = original_keyframe.easing
-                                keyframe.amplitude = original_keyframe.amplitude
-                                keyframe.back = original_keyframe.back
-                                keyframe.period = original_keyframe.period
-                                keyframe.type = original_keyframe.type
+                    key_properties = {prop.identifier: getattr(keyframe, prop.identifier) for prop in keyframe.bl_rna.properties if not prop.is_readonly}
+                    original_keyframes[int(keyframe.co[0])] = key_properties
 
-                                keyframe.handle_left = original_keyframe.handle_left
-                                keyframe.handle_left_type = original_keyframe.handle_left_type
-                                keyframe.handle_right = original_keyframe.handle_right
-                                keyframe.handle_right_type = original_keyframe.handle_right_type
-                                break
+                # get_original_modifiers
+                original_modifiers = {}
+                for mod in fcurve.modifiers:
+                    mod_properties = {prop.identifier: getattr(mod, prop.identifier) for prop in mod.bl_rna.properties if not prop.is_readonly}
+                    original_modifiers[mod.type] = mod_properties
+
+
+                for target in targets:
+                    target_fcurve = anim_data.action.fcurves.new(f'key_blocks["{target.name}"].value', action_group=original_fcurve_group)
+
+                    # copy_fcurve_properties
+                    for prop, value in original_fcurve_properties.items():
+                        if hasattr(fcurve, prop):
+                            if prop not in ("data_path", "group", "is_valid"):
+                                setattr(target_fcurve, prop, value)
+
+                    # copy_keyframes
+                    for keyframe, properties in original_keyframes.items():
+                        new_key = target_fcurve.keyframe_points.insert(keyframe, properties["co"][1])
+                        for prop, value in properties.items():
+                            if hasattr(new_key, prop):
+                                setattr(new_key, prop, value)
+
+                    # copy_modifiers
+                    for mod, properties in original_modifiers.items():
+                        new_mod = target_fcurve.modifiers.new(mod)
+                        for prop, value in properties.items():
+                            if hasattr(new_mod, prop):
+                                setattr(new_mod, prop, value)
+                break
 
     # Transfer Drivers
     for driver in anim_data.drivers:
@@ -48,3 +62,4 @@ def transfer_animation(shape_keys, source, *targets):
             for target in targets:
                 new_driver = anim_data.drivers.from_existing(src_driver=driver)
                 new_driver.data_path = f'key_blocks["{target.name}"].value'
+            break
