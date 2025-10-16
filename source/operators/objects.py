339ad54b591e0e6d38baa67b30dea35e60cb05ab
Nika Kutsniashvili <nickberckley@gmail.com>
2025-10-17 00:07:27 +0400

    Refactor "Objects from Shape Keys" operator
    - New, fool-proof method of detecting duplicates that doesn't depend on naming conventions anymore. Instead, shape key values are cached and compared directly.
    - Because of the above change, comparing to existing objects in the scene is separated and is a new option (off by default). When enabled, vertex positions of other similar objects are cached and compared to newly created objects.
    - Improved UI and printing.

--- source/operators/objects.py
+++ source/operators/objects.py
@@ -1,4 +1,5 @@
 import bpy
+import numpy
 
 from ..functions.poll import (
     shape_key_poll,
@@ -11,39 +12,49 @@ from ..functions.poll import (
 class OBJECT_OT_objects_from_shape_keys(bpy.types.Operator):
     bl_idname = "object.objects_from_shape_keys"
     bl_label = "Objects from Shape Keys"
-    bl_description = ("Creates new object for every frame that has different Shape Key values within the selected frame range.\n"
-                      "Can be used to turn keyframed mesh into separate objects.\nDuplicated meshes will be given unique name for origanizational purposes")
+    bl_description = ("Iterates over a given frame range and creates a new object on every frame\n"
+                      "if the shape key values are different from the previous frame, i.e. mesh is different")
     bl_options = {'REGISTER', 'UNDO'}
 
     delete_duplicates: bpy.props.BoolProperty(
         name="Delete Duplicates",
-        description="If duplicated object is exact copy of previous one it will be deleted automatically",
-        default=True
+        description="Do not create a new object if the object with the same exact shape already exists",
+        default=True,
+    )
+    consider_existing_objects: bpy.props.BoolProperty(
+        name="Consider Existing Objects",
+        description=("When checking for duplicates, consider objects that already exist in the scene.\n"
+                     "If an object with the exact same mesh already exists in the scene, do not create a new one.\n"
+                     "This is most useful when re-running the operator on the same object multiple times"),
+        default=False,
     )
 
     start_frame: bpy.props.IntProperty(
         name="Start Frame",
-        default=1, min=1
+        min=1,
+        default=1,
     )
     end_frame: bpy.props.IntProperty(
         name="End Frame",
-        default=10, min=1
+        min=1,
+        default=10,
     )
     step: bpy.props.IntProperty(
         name="Step",
-        default=1, min=1, max=4
+        min=1, max=4,
+        default=1,
     )
 
     keyframes_only: bpy.props.BoolProperty(
         name="Keyframes Only",
         description="Duplicate object only on keyframed frames instead of on every frame that has different shape key values",
-        default=False
+        default=False,
     )
 
     keep_position: bpy.props.BoolProperty(
         name="Keep Position",
         description="If enabled duplicated objects will have same position as original. Otherwise they'll move along the selected axis",
-        default=False
+        default=False,
     )
     move_axis: bpy.props.EnumProperty(
         name="Move on Axis",
@@ -57,13 +68,13 @@ class OBJECT_OT_objects_from_shape_keys(bpy.types.Operator):
         name="Offset",
         description="Distance to move each object by",
         subtype='DISTANCE', unit='LENGTH',
-        default=2.0
+        default=2.0,
     )
 
     hide_duplicates: bpy.props.BoolProperty(
         name="Hide Duplicates",
         description="If enabled collection containing newly created objects will be hidden in the viewport",
-        default=False
+        default=False,
     )
 
     @classmethod
@@ -82,105 +93,106 @@ class OBJECT_OT_objects_from_shape_keys(bpy.types.Operator):
         layout.use_property_split = True
         layout.use_property_decorate = False
 
-        layout.prop(self, "delete_duplicates")
         col = layout.column(align=True)
+        col.prop(self, "delete_duplicates")
+        if self.delete_duplicates:
+            col.prop(self, "consider_existing_objects")
+        col.separator()
+
+        col = layout.column(align=False)
         row = col.row()
         row.prop(self, "start_frame", text="Frame Range")
         row.prop(self, "end_frame", text="")
-        layout.prop(self, "step")
+        col.prop(self, "step")
 
-        layout.prop(self, "keyframes_only")
-        layout.separator()
+        col.prop(self, "keyframes_only")
+        col.separator()
 
-        layout.prop(self, "keep_position")
-        col2 = layout.column(align=True)
-        row = col2.row(align=True)
+        col = layout.column(align=True)
+        col.prop(self, "keep_position")
+        axis_col = layout.column(align=True)
+        row = axis_col.row(align=True)
         row.prop(self, "move_axis", expand=True)
-        col2.prop(self, "offset_distance")
-        layout.prop(self, "hide_duplicates")
-
+        axis_col.prop(self, "offset_distance")
         if self.keep_position:
-            col2.enabled = False
+            axis_col.enabled = False
+
+        layout.prop(self, "hide_duplicates")
 
 
     def invoke(self, context, event):
+        self.start_frame = context.scene.frame_start
+        self.end_frame = context.scene.frame_end
+
         return context.window_manager.invoke_props_dialog(self)
 
 
     def execute(self, context):
-        obj = context.active_object
+        obj = context.object
         move_axis_index = 'XYZ'.index(self.move_axis)
+        print("Starting 'Objects from Shape Keys' operator")
 
-        # Create Collection
+        # Create the Collection
         duplicates_collection = bpy.data.collections.new(obj.name + "_duplicates")
         obj.users_collection[0].children.link(duplicates_collection)
 
-        # Define Timeline
+        # define_frame_range
         if self.keyframes_only:
-            keyframe_frames = set()
+            frame_range = set()
             for fcurve in obj.data.shape_keys.animation_data.action.fcurves:
                 if fcurve.data_path.startswith("key_blocks"):
-                    keyframe_frames.update(int(keyframe.co[0]) for keyframe in fcurve.keyframe_points)
-            keyframe_frames &= set(range(self.start_frame, self.end_frame + 1, self.step))
+                    frame_range.update(int(keyframe.co[0]) for keyframe in fcurve.keyframe_points)
+            frame_range &= set(range(self.start_frame, self.end_frame + 1, self.step))
         else:
-            keyframe_frames = range(self.start_frame, self.end_frame + 1, self.step)
+            frame_range = range(self.start_frame, self.end_frame + 1, self.step)
 
-        # Iteration
+        # Cache meshes in the scene
+        scene_objects_cache = {}
+        if self.consider_existing_objects:
+            scene_objects_cache = self._cache_existing_objects(context, obj)
+
+        unique_shape_keys_dict = {}
         prev_obj = None
-        for frame in sorted(keyframe_frames):
+        for frame in sorted(frame_range):
             context.scene.frame_set(frame)
 
-            # Duplicating the Object
-            obj_copy = obj.copy()
-            obj_copy.data = obj.data.copy()
-            obj_copy.name = obj.name + "_frame_" + str(frame)
-
-            context.collection.objects.link(obj_copy)
-            bpy.ops.object.select_all(action='DESELECT')
-            obj_copy.select_set(True)
-            context.view_layer.objects.active = obj_copy
-
-            # Applying Shape Keys
-            bpy.ops.object.shape_key_remove(all=True, apply_mix=True)
-            obj_copy.select_set(False)
-
-            # Mesh Naming Convention
-            name = ''.join([self._get_name(key) for key in obj.data.shape_keys.key_blocks if key.name != 'Basis'])
+            # Detect Duplicate
+            match = None
             if self.delete_duplicates:
-                data_type = type(obj.data)
-                if data_type == bpy.types.Mesh:
-                    if bpy.data.meshes.get(name) is not None:
-                        bpy.data.objects.remove(obj_copy)
-                        continue
-                elif data_type == bpy.types.Curve:
-                    if bpy.data.curves.get(name) is not None:
-                        obj = bpy.data.objects.get(name)
-                        bpy.data.objects.delete(obj_copy)
-                        continue
-                elif data_type == bpy.types.Lattice:
-                    if bpy.data.lattices.get(name) is not None:
-                        bpy.data.objects.remove(obj_copy)
-                        continue
+                sk_values, match = self._detect_duplicate(context, obj, obj.data, unique_shape_keys_dict, scene_objects_cache)
 
-            if len(name) < 6:
-                obj_copy.data.name = obj.name + "_shape_key_" + name
+            if match:
+                print(f"- Duplicate detected on the frame {frame}. Matching object: {match.name}")
             else:
-                obj_copy.data.name = name
+                # Duplicate object
+                obj_copy = obj.copy()
+                obj_copy.data = obj.data.copy()
+                obj_copy.name = obj_copy.data.name = obj.name + "_frame_" + str(frame)
+                duplicates_collection.objects.link(obj_copy)
 
-            # Move to Collection
-            duplicates_collection.objects.link(obj_copy)
-            for coll in obj_copy.users_collection:
-                if coll != duplicates_collection:
-                    coll.objects.unlink(obj_copy)
+                # Cache shape key values to the dict of uniques
+                if self.delete_duplicates:
+                    unique_shape_keys_dict[sk_values] = obj_copy
 
-            # Offset Duplicates
-            if not self.keep_position:
-                if prev_obj is not None:
-                    obj_copy.location[move_axis_index] = prev_obj.location[move_axis_index] + self.offset_distance
-                prev_obj = obj_copy
+                # Apply shape keys
+                bpy.ops.object.select_all(action='DESELECT')
+                obj_copy.select_set(True)
+                context.view_layer.objects.active = obj_copy
 
-        # Reset Everything
-        bpy.context.view_layer.objects.active = obj
+                area = [area for area in context.screen.areas if area.type == 'VIEW_3D'][0]
+                with context.temp_override(area=area):
+                    bpy.ops.object.shape_key_remove(all=True, apply_mix=True)
+
+                obj_copy.select_set(False)
+
+                # Offset from the previous duplicate
+                if not self.keep_position:
+                    if prev_obj is not None:
+                        obj_copy.location[move_axis_index] = prev_obj.location[move_axis_index] + self.offset_distance
+                    prev_obj = obj_copy
+
+        # Reset everything
+        context.view_layer.objects.active = obj
         obj.select_set(True)
         context.scene.frame_set(self.start_frame)
         if self.hide_duplicates:
@@ -188,22 +200,70 @@ class OBJECT_OT_objects_from_shape_keys(bpy.types.Operator):
 
         # Report
         num_duplicates = len(duplicates_collection.objects)
-        self.report({'INFO'}, "{} objects created".format(num_duplicates))
+        self.report({'INFO'}, f"{num_duplicates} objects created. Check console for details about duplicates")
         obj.hide_set(True)
 
         return {'FINISHED'}
 
 
-    def _get_name(self, key):
-        value = key.value
-        if value.is_integer():
-            return chr(int(value) + 65)
-        elif value < 1:
-            return str(int(value * 100))
-        else:
-            integer_part = chr(int(value) + 64)
-            decimal_part = str(int((value % 1) * 100))
-            return f"{integer_part}{decimal_part}"
+    def _cache_existing_objects(self, context, active_obj):
+        """Creates a dictionary of existing mesh objects in the scene and numpy arrays of their vertex positions."""
+
+        # Evaluate active object to get it's vertex count
+        depsgraph = context.evaluated_depsgraph_get()
+        eval_active_obj = active_obj.evaluated_get(depsgraph)
+        eval_active_obj_vert_count = len(eval_active_obj.data.vertices)
+
+        scene_objects_cache = {}
+        for obj in context.scene.objects:
+            if obj == active_obj:
+                continue
+            if obj.type != active_obj.type:
+                continue
+
+            eval_obj = obj.evaluated_get(depsgraph)
+
+            # Skip objects with different vertex count than the active object
+            eval_obj_vert_count = len(eval_obj.data.vertices)
+            if eval_obj_vert_count != eval_active_obj_vert_count:
+                continue
+
+            verts_co = numpy.empty((eval_obj_vert_count * 3), dtype=numpy.float64)
+            eval_obj.data.vertices.foreach_get("co", verts_co)
+
+            scene_objects_cache[obj] = verts_co
+
+        return scene_objects_cache
+
+
+    def _detect_duplicate(self, context, obj, data, unique_shape_keys_dict, scene_objects_cache):
+        """Checks if the exact match of the evaluated mesh (on the current frame) has already been created in loop (or exists in the scene)."""
+        """Compares array of evaluated meshes vertex positions to other arrays in `unique_verts_dict` and returns match if found."""
+        """If match is not found array is added to the dict as an unique."""
+
+        match = None
+        sk_values = None
+        verts_co = None
+
+        # Compare current shape key values to previously stored ones
+        sk_values = tuple(key.value for key in data.shape_keys.key_blocks)
+        if sk_values in unique_shape_keys_dict:
+            match = unique_shape_keys_dict[sk_values]
+
+        # Compare to existing objects in the scene
+        if self.consider_existing_objects:
+            depsgraph = context.evaluated_depsgraph_get()
+            eval_obj = obj.evaluated_get(depsgraph)
+
+            verts_co = numpy.empty((len(eval_obj.data.vertices) * 3), dtype=numpy.float64)
+            eval_obj.data.vertices.foreach_get("co", verts_co)
+
+            for key, values in scene_objects_cache.items():
+                if numpy.array_equal(verts_co, values):
+                    match = key
+                    break
+
+        return sk_values, match
 
 
 
